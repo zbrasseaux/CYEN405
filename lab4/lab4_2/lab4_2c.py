5a00eb533fb610d2754f887aa465aa843a0080bc
EC2 Default User <ec2-user@ip-172-31-43-238.us-east-2.compute.internal>
2019-11-01 18:17:49 +0000

    bucket name change

--- lab4/lab4_2/lab4_2c.py
+++ lab4/lab4_2/lab4_2c.py
@@ -7,7 +7,7 @@ import boto3
 from botocore.exceptions import ClientError
 
 filename="lab4_2a.py"
-bucket_name = 'zpb004lab42buc'
+bucket_name = 'zpb004lab42bucket'
 
 s3_client = boto3.client('s3')
 s3_client.upload_file(filename, bucket_name, filename)
